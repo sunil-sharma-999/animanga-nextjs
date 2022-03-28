f48812d55d3a64112145092c0d42105dd78fd83c
sunil-sharma-999 <84917190+sunil-sharma-999@users.noreply.github.com>
2022-03-28 20:41:33 +0530

    Delete helper directory

--- helper/deleteReview.js
+++
@@ -1,9 +0,0 @@
-import { deleteField, doc, updateDoc } from '@firebase/firestore';
-import { db } from '../firebase';
-
-export const deleteReview = async ({ collectionName, authState }) => {
-  const docRef = doc(db, 'reviews', collectionName);
-  return await updateDoc(docRef, {
-    [authState]: deleteField(),
-  });
-};
